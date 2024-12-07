c0b152414a6694515c2aea790b643ef234c0269a
Denys Deren <slden.studio.oficial@gmail.com>
2024-12-07 13:52:29 +0100

    third

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
     def greeting(self):
-        print("Hello, nice to meet you!")
+        print(f"hello I am {self.name}")
 
-first_student = Student()
+    def grow_up(self):
+        self.age = self.age + 1
+    
+    def print_age(self):
+        print(f"age - {self.age}")
 
-first_student.greeting()
+
+maxym_student = Student(name = "Maxym", age=12)
+maxym_student.greeting()
+
+matvii_stydent = Student(name = "Matvii", age=14)
+matvii_stydent.greeting()
+
+matvii_stydent.print_age()
+matvii_stydent.grow_up()
+matvii_stydent.print_age()
+
+maxym_student.print_age()
